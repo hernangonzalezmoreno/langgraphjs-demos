ed917bdf4339bbe82e5f4c92c2d57e5935358537
Hernán González Moreno <hernangonzalezmoreno@gmail.com>
2025-01-30 16:02:20 -0300

    feat: instalacion de paquetes

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
 import 'dotenv/config';
 
-console.log(process.env.HELLO);
+console.log('BASE_URL_OLLAMA', process.env.BASE_URL_OLLAMA);
+console.log('LANGCHAIN_API_KEY', process.env.LANGCHAIN_API_KEY);
+console.log('LANGCHAIN_TRACING_V2', process.env.LANGCHAIN_TRACING_V2);
+console.log('LANGCHAIN_PROJECT', process.env.LANGCHAIN_PROJECT);
