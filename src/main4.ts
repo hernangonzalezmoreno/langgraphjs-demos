1140d39951b342802ff0bdb5e82b38c1704338b0
Hernán González Moreno <hernangonzalezmoreno@gmail.com>
2025-01-31 16:11:07 -0300

    feat(main4.ts): ejemplo de 'parallelization'

---
+++ src/main4.ts
@@ -0,0 +1,79 @@
+import 'dotenv/config';
+import { ChatOllama } from '@langchain/ollama';
+import { StateGraph, Annotation } from "@langchain/langgraph";
+import MermaidGraph from './MermaidGraph';
+
+console.log('BASE_URL_OLLAMA', process.env.BASE_URL_OLLAMA);
+console.log('TAVILY_API_KEY', process.env.TAVILY_API_KEY);
+console.log('LANGCHAIN_API_KEY', process.env.LANGCHAIN_API_KEY);
+console.log('LANGCHAIN_TRACING_V2', process.env.LANGCHAIN_TRACING_V2);
+console.log('LANGCHAIN_PROJECT', process.env.LANGCHAIN_PROJECT);
+
+async function main() {
+  const llm = new ChatOllama({
+    model: 'llama3.1:8b',
+    temperature: 0.1,
+    baseUrl: process.env.BASE_URL_OLLAMA,
+  });
+
+  // Graph state
+  const StateAnnotation = Annotation.Root({
+    topic: Annotation<string>,
+    joke: Annotation<string>,
+    story: Annotation<string>,
+    poem: Annotation<string>,
+    combinedOutput: Annotation<string>,
+  });
+
+  // Nodes
+  // First LLM call to generate initial joke
+  async function callLlm1(state: typeof StateAnnotation.State) {
+    const msg = await llm.invoke(`Write a joke about ${state.topic}`);
+    return { joke: msg.content };
+  }
+
+  // Second LLM call to generate story
+  async function callLlm2(state: typeof StateAnnotation.State) {
+    const msg = await llm.invoke(`Write a story about ${state.topic}`);
+    return { story: msg.content };
+  }
+
+  // Third LLM call to generate poem
+  async function callLlm3(state: typeof StateAnnotation.State) {
+    const msg = await llm.invoke(`Write a poem about ${state.topic}`);
+    return { poem: msg.content };
+  }
+
+  // Combine the joke, story and poem into a single output
+  async function aggregator(state: typeof StateAnnotation.State) {
+    const combined = `Here's a story, joke, and poem about ${state.topic}!\n\n` +
+      `STORY:\n${state.story}\n\n` +
+      `JOKE:\n${state.joke}\n\n` +
+      `POEM:\n${state.poem}`;
+    return { combinedOutput: combined };
+  }
+
+  // Build workflow
+  const parallelWorkflow = new StateGraph(StateAnnotation)
+    .addNode("callLlm1", callLlm1)
+    .addNode("callLlm2", callLlm2)
+    .addNode("callLlm3", callLlm3)
+    .addNode("aggregator", aggregator)
+    .addEdge("__start__", "callLlm1")
+    .addEdge("__start__", "callLlm2")
+    .addEdge("__start__", "callLlm3")
+    .addEdge("callLlm1", "aggregator")
+    .addEdge("callLlm2", "aggregator")
+    .addEdge("callLlm3", "aggregator")
+    .addEdge("aggregator", "__end__")
+    .compile();
+
+  // Draw the agent graph
+  MermaidGraph.drawMermaidAsImage(parallelWorkflow);
+
+  // Invoke
+  const result = await parallelWorkflow.invoke({ topic: "cats" });
+  console.log(result.combinedOutput);
+}
+
+main();
