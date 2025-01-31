15eaa86155bbf10fb3e9797c2a152c2ae968a97f
Hernán González Moreno <hernangonzalezmoreno@gmail.com>
2025-01-31 08:31:58 -0300

    feat(agent): construcción de un agente simple

---
+++ src/agent.ts
@@ -0,0 +1,23 @@
+import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
+import { ChatOllama } from "@langchain/ollama";
+import { CompiledStateGraph, MemorySaver } from "@langchain/langgraph";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+
+export function simpleAgent(): CompiledStateGraph<any, any>{
+  // Define the tools for the agent to use
+  const agentTools = [new TavilySearchResults({ maxResults: 3 })];
+  const agentModel = new ChatOllama({
+    model: 'llama3.1:8b',
+    temperature: 0.1,
+    baseUrl: process.env.BASE_URL_OLLAMA,
+  });
+
+  // Initialize memory to persist state between graph runs
+  const agentCheckpointer = new MemorySaver();
+  const agent = createReactAgent({
+    llm: agentModel,
+    tools: agentTools,
+    checkpointSaver: agentCheckpointer,
+  });
+  return agent;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,34 @@
 import 'dotenv/config';
+import { simpleAgent } from './agent';
+import { HumanMessage } from '@langchain/core/messages';
 
 console.log('BASE_URL_OLLAMA', process.env.BASE_URL_OLLAMA);
+console.log('TAVILY_API_KEY', process.env.TAVILY_API_KEY);
 console.log('LANGCHAIN_API_KEY', process.env.LANGCHAIN_API_KEY);
 console.log('LANGCHAIN_TRACING_V2', process.env.LANGCHAIN_TRACING_V2);
 console.log('LANGCHAIN_PROJECT', process.env.LANGCHAIN_PROJECT);
+
+async function main() {
+  const agent = simpleAgent();
+
+  // Now it's time to use!
+  const agentFinalState = await agent.invoke(
+    { messages: [new HumanMessage("¿Quien es Hernan Gonzalez Moreno? Buscalo en github y en Linkedin. Dame información de él. Responde en español.")] },
+    { configurable: { thread_id: "42" } },
+  );
+
+  console.log(
+    agentFinalState.messages[agentFinalState.messages.length - 1].content,
+  );
+
+  const agentNextState = await agent.invoke(
+    { messages: [new HumanMessage("¿Sabes donde nacio? ¿O donde estudio?")] },
+    { configurable: { thread_id: "42" } },
+  );
+
+  console.log(
+    agentNextState.messages[agentNextState.messages.length - 1].content,
+  );
+}
+
+main();
